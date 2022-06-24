ebb5f397931e811bd8274d2eb617806e758edc32
DoctorLector <roller97@yandex.ru>
2022-06-24 21:37:27 +0300

    add code habr 21 lecon part 2

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
+import json
+from time import time
 
 
 class SearchableMixin(object):
@@ -81,8 +83,11 @@ class User(UserMixin, db.Model):
                                     backref='author', lazy='dynamic')
 
     messages_received = db.relationship('Message',
-                                        foreign_keys='Messgaes.recipient_id',
+                                        foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -137,6 +142,12 @@ class User(UserMixin, db.Model):
             Message.timestamp > last_read_time
         ).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -165,3 +176,12 @@ class Message(db.Model):
     def __repr__(self):
         return '<Message {}>'.format(self.body)
 
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
